9af3c35f14ddb999dcdb35f50efe2f5992d81871
Franck Monge <franckyroquai@gmail.com>
2023-06-26 15:07:18 +0200

    ajout dossier data array liste de films

---
+++ data/films.js
@@ -0,0 +1,57 @@
+var filmData = [
+    {
+        title : "Matrix" ,
+        image : "images/matrix.jpg" ,
+        text : `Programmeur anonyme dans un service administratif le jour, Thomas Anderson devient Neo la nuit venue. Sous ce pseudonyme, il est l'un des pirates les plus recherchés du cyber-espace. À cheval entre deux mondes, Neo est assailli par d'étranges songes et des messages cryptés provenant d'un certain Morpheus. Celui-ci l'exhorte à aller au-delà des apparences et à trouver la réponse à la question qui hante constamment ses pensées : qu'est-ce que la Matrice ?`
+    },
+    {
+        title : "Le Bon, La Brute et le Truand" ,
+        image : "images/bonbrutetruand.jpg" ,
+        text : `Pendant la Guerre de Sécession, trois hommes, préférant s'intéresser à leur profit personnel, se lancent à la recherche d'un coffre contenant 200 000 dollars en pièces d'or volés à l'armée sudiste. Tuco sait que le trésor se trouve dans un cimetière, tandis que Joe connaît le nom inscrit sur la pierre tombale qui sert de cache. Chacun a besoin de l'autre, mais un troisième homme entre dans la course: Setenza, un homme qui n'hésite pas à massacrer femmes et enfants pour parvenir à ses fins.`
+    },
+    {
+        title : "Black Swan" ,
+        image : "images/BlackSwan.webp" ,
+        text : `Au début c'était un canard !!!`
+    },
+    {
+        title : "E.T." ,
+        image : "images/et.jpg" ,
+        text : `Une soucoupe volante atterrit en pleine nuit près de Los Angeles. Quelques extraterrestres, envoyés sur Terre en mission d'exploration botanique, sortent de l'engin, mais un des leurs s'aventure au-delà de la clairière où se trouve la navette. Celui-ci se dirige alors vers la ville. C'est sa première découverte de la civilisation humaine. Bientôt traquée par des militaires et abandonnée par les siens, cette petite créature apeurée se nommant E.T. se réfugie dans une résidence de banlieue.`
+    },
+    {
+        title : "Les Dents de la Mer" ,
+        image : "images/jaws.jpg" ,
+        text : `À quelques jours du début de la saison estivale, les habitants de la petite station balnéaire d'Amity sont en émoi face à la découverte, sur le littoral, du corps atrocement mutilé d'une jeune vacancière. Pour Martin Brody, le chef de la police, il ne fait aucun doute que la jeune fille a été victime d'un requin. Il décide alors d'interdire l'accès des plages mais se heurte à l'hostilité du maire, uniquement intéressé par l'afflux des touristes.`
+    },
+    {
+        title : "Kill Bill" ,
+        image : "images/killbill.jpg" ,
+        text : `Au cours d'une cérémonie de mariage en plein désert, un commando fait irruption dans la chapelle et tire sur les convives. Laissée pour morte, la mariée enceinte retrouve ses esprits après un coma de quatre ans. Celle qui a auparavant exercé les fonctions de tueuse à gages au sein du Détachement international des Vipères assassines n'a alors plus qu'une seule idée en tête: venger la mort de ses proches en éliminant tous les membres de cette organisation criminelle.`
+    },
+    {
+        title : "Le Mandalorien" ,
+        image : "images/Mandalorian_1200.jpg" ,
+        text : `Bon ok c'est une série mais vous voulez vraiment qu'on parle de la postlogie ???`
+    },
+    {
+        title : "Moonraker" ,
+        image : "images/moonraker.avif" ,
+        text : `L'agent secret britannique James Bond enquête sur la disparition d'une navette spatiale americaine, Moonraker, confiee au gouvernement britannique. 007 se rend aux Etats-Unis pour interroger le responsable de la construction de la navette, Sir Hugo Drax. Il y fait la rencontre de la charmante Holly Goodhead et decouvre que Drax est en fait le responsable de la disparition de la navette.`
+    },
+    {
+        title : "Rambo 2 : La Mission" ,
+        image : "images/rambo2.jpg" ,
+        text : `Ex-combattant du Viêtnam, John Rambo purge une peine de cinq années de prison. Son ancien colonel lui offre une chance de se racheter : il devra retourner au Viêtnam et repérer les camps détenant des prisonniers américains depuis la fin de la guerre. Rambo prend sa tâche très au sérieux et ne tarde pas à découvrir quelques compatriotes gémissant sous le joug de tortionnaires communistes.`
+    },
+    {
+        title : "Star Wars" ,
+        image : "images/starwars.jpg" ,
+        text : `La guerre civile fait rage entre l'Empire galactique et l'Alliance rebelle. Capturée par les troupes de choc de l'Empereur menées par le sombre et impitoyable Dark Vador, la princesse Leia Organa dissimule les plans de l'Etoile Noire.`
+    },
+    {
+        title : "Wonder Woman" ,
+        image : "images/wonderwoman.jpg" ,
+        text : `C'était avant qu'elle ne devienne Wonder Woman, à l'époque où elle était encore Diana, princesse des Amazones et combattante invincible. Un jour, un pilote américain s'écrase sur l'île paradisiaque où elle vit, à l'abri des fracas du monde. Lorsqu'il lui raconte qu'une guerre terrible fait rage à l'autre bout de la planète, Diana quitte son havre de paix, convaincue qu'elle doit enrayer la menace.`
+    }
+]
